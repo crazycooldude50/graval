da57f206c87a1ce8857936acdb96e27162e8d76c
Cloudz <hitaansh1912@gmail.com>
2023-02-17 13:58:11 -0800

    Update Door.cs

--- game design/Assets/Scripts/Door.cs
+++ game design/Assets/Scripts/Door.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class Door : MonoBehaviour
 {
+    public GameObject door;
     // Start is called before the first frame update
     void Start()
     {
-
+        door.SetActive(true);
     }
 
     // Update is called once per frame
