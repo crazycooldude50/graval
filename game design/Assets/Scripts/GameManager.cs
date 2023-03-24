cfe3d1bbf38d414dd093aea1763392c0d5a3a4bd
Cloudz <hitaansh1912@gmail.com>
2023-03-24 14:01:38 -0700

    HJ 3/24 gamemanager restart button

--- game design/Assets/Scripts/GameManager.cs
+++ game design/Assets/Scripts/GameManager.cs
@@ -6,14 +6,32 @@ public class GameManager : MonoBehaviour
 {
     private int roomNumber;
     public GameObject player;
-    public GameObject roomTrigger;
+    public GameObject rooms;
     private PlayerController playerScript;
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
-
         playerScript = player.GetComponent<PlayerController>();
+        rooms = GameObject.Find("Rooms");
+
+        List<Dictionary<GameObject, Vector3>> roomStates = new List<Dictionary<GameObject, Vector3>>();
+        for(int j = 0; j < rooms.transform.childCount; j++)
+        {
+            //roomStates.Add(new Dictionary<GameObject, Vector3>);
+            for (int i = 0; i < rooms.transform.GetChild(j).transform.childCount; i++)
+            {
+                
+                GameObject child = rooms.transform.GetChild(j).GetChild(i).gameObject;
+                Debug.Log(roomStates);
+                Debug.Log(child.name.Substring(0, 21));
+                if (child.name.Substring(0, 19).Equals("Controllable Object") || child.name.Substring(0, 21).Equals("UnControllable Object"))
+                {
+                   // roomStates.Set(i[child] = child.transform.position);
+
+                }
+            }
+        }
 
     }
 
@@ -23,12 +41,16 @@ public class GameManager : MonoBehaviour
         roomNumber = playerScript.roomNumber;
         if(Input.GetKeyDown("R"))
         {
-            roomTrigger = GameObject.Find("Room Trigger " + roomNumber);
-            player.position.transform()
+            //roomTrigger = GameObject.Find("Room Trigger " + roomNumber);
+            //player.position.transform();
         }
     }
     void ResetLevel()
     {
         
+    }
+    void LoadScene()
+    {
+
     }
 }
