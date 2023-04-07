a679a4b82288d8ea4c739723201e9cb1e9a8b6ee
Goodman599 <62830208+Goodman599@users.noreply.github.com>
2023-04-07 13:58:31 -0700

    worked on camera controller HM 4/7

--- game design/Assets/Scripts/CameraController.cs
+++ game design/Assets/Scripts/CameraController.cs
@@ -5,15 +5,71 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private GameObject player;
+    private GameObject triggers;
+    private PlayerController playerScript;
+    [SerializeField] private float xBoundLeft;
+    [SerializeField] private float xBoundRight;
+    [SerializeField] private float width;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.Find("Player");
+        triggers = GameObject.Find("Triggers");
+        playerScript = player.GetComponent<PlayerController>();
+        Camera cam = Camera.main;
+        float height = cam.orthographicSize;
+        width = height * cam.aspect;
     }
 
     // Update is called once per frame
     void Update()
     {
-        transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        // If in first room, get custom camera starting pos
+        if (playerScript.roomNumber == 1)
+        {
+            xBoundLeft = GameObject.Find("Camera Start").transform.position.x;
+        }
+        else
+        {
+            xBoundLeft = triggers.transform.GetChild(playerScript.roomNumber - 1).position.x;
+        }
+
+        // If in last room, get custom ending pos
+        if (playerScript.roomNumber == triggers.transform.childCount)
+        {
+            xBoundRight = GameObject.Find("Camera End").transform.position.x;
+        }
+        else
+        {
+            xBoundRight = triggers.transform.GetChild(playerScript.roomNumber).position.x;
+        }
+
+        // Account for width of camera view in bounds
+        xBoundLeft += width;
+        xBoundRight -= width;
+
+        // Find center point if left bound is to the right of the right bound
+        if (xBoundLeft > xBoundRight)
+        {
+            xBoundLeft += xBoundRight;
+            xBoundLeft /= 2;
+            xBoundRight = xBoundLeft;
+        }
+
+        // Convert world coordinate to camera pos
+        xBoundLeft -= 29;
+        xBoundRight -= 27;
+
+        // If in bounds follow player
+        if (transform.position.x > xBoundLeft && transform.position.x < xBoundRight)
+        {
+            transform.position = new Vector3(player.transform.position.x, transform.position.y, transform.position.z);
+        }
+        else
+        {
+            transform.position = new Vector3(xBoundRight + 28, transform.position.y, transform.position.z);
+        }
+        
     }
 }
